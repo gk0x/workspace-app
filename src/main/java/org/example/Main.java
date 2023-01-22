09556fc0a834d773b1145c9c6f4964c8939c642f
Grzegorz Klusek <gk0x@int.pl>
2023-01-22 18:56:12 +0100

    add a datebase connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String url = "jdbc:mysql://localhost:3306/workspace";
+        String user = "root"; // nazwa użytkownika
+        String password = ""; // hasło
+
+        try {
+            // Połączenie z bazą danych
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Połączenie z bazą danych udane.");
+
+            // Tutaj można wykonywać operacje na bazie danych, np. wykonywać zapytania SQL
+
+
+            // Zamykanie połączenia
+            con.close();
+            System.out.println("Połączenie z bazą danych zamknięte.");
+        } catch (SQLException e) {
+            System.out.println("Wystąpił błąd podczas połączenia z bazą danych: " + e.getMessage());
+        }
     }
 }
