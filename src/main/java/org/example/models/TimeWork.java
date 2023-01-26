2a86e9da6c99a3500f2f71790c134e0d1f8f9fbf
Grzegorz Klusek <gk0x@int.pl>
2023-01-26 19:46:41 +0100

    add class TimeWork with CRUD methods

---
+++ src/main/java/org/example/models/TimeWork.java
@@ -0,0 +1,75 @@
+package org.example.models;
+
+import java.sql.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class TimeWork {
+    private int id;
+    private int employeeId;
+    private LocalDateTime loginTime;
+    private LocalDateTime logoutTime;
+    private int projectId;
+    private LocalTime workingTime;
+
+    public TimeWork() {
+    }
+
+    public TimeWork(int id, int employeeId, LocalDateTime loginTime, LocalDateTime logoutTime, int projectId, LocalTime workingTime) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.loginTime = loginTime;
+        this.logoutTime = logoutTime;
+        this.projectId = projectId;
+        this.workingTime = workingTime;
+    }
+
+    public void CreateTimeWork(Connection con, int employeeId, LocalDateTime loginTime, LocalDateTime logoutTime, int projectId) {
+        try {
+            String sql = "INSERT INTO time_work (id_pracownika, data_zalogowania, data_wylogowania, id_projektu) VALUES (?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, employeeId);
+            ps.setTimestamp(2, Timestamp.valueOf(loginTime));
+            ps.setTimestamp(3, Timestamp.valueOf(logoutTime));
+            ps.setInt(4, projectId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public TimeWork getTimeWork(Connection con, int id) {
+        TimeWork timeWork = null;
+        try {
+            String sql = "SELECT * FROM czas_pracy WHERE id = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int employeeId = rs.getInt("id_pracownika");
+                LocalDateTime loginTime = rs.getTimestamp("data_zalogowania").toLocalDateTime();
+                LocalDateTime logoutTime = rs.getTimestamp("data_wylogowania").toLocalDateTime();
+                int projectId = rs.getInt("id_projektu");
+
+                Duration durationBetweenLoginLogout = Duration.between(loginTime, logoutTime);
+                long seconds = durationBetweenLoginLogout.getSeconds();
+                LocalTime workingTime = LocalTime.ofSecondOfDay(seconds);
+                timeWork = new TimeWork(id, employeeId, loginTime, logoutTime, projectId, workingTime);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return timeWork;
+    }
+
+
+}
+
+
