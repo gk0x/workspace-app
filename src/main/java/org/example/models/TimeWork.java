a6e60c10e3273be5e9e0151f4039cd7b71a38d25
Grzegorz Klusek <gk0x@int.pl>
2023-01-27 20:20:04 +0100

    create methods for counting time

--- src/main/java/org/example/models/TimeWork.java
+++ src/main/java/org/example/models/TimeWork.java
@@ -1,12 +1,11 @@
 package org.example.models;
 
-import com.mysql.cj.jdbc.DatabaseMetaData;
-
-import javax.sql.DataSource;
 import java.sql.*;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class TimeWork {
     private int id;
@@ -27,6 +26,9 @@ public class TimeWork {
         this.projectId = projectId;
         this.workingTime = workingTime;
     }
+
+
+
 
     public void CreateTimeWork(Connection con, int employeeId, LocalDateTime loginTime, LocalDateTime logoutTime, int projectId) {
         try {
@@ -71,29 +73,94 @@ public class TimeWork {
         }
         return timeWork;
     }
-    public void insertTimeWork(int employeeId, LocalDateTime loginTime, LocalDateTime logoutTime, int projectId) {
 
 
-        String sql = "INSERT INTO czas_pracy (id_pracownika, data_zalogowania, data_wylogowania, id_projektu, czas) VALUES (?,?,?,?,?)";
-        DataSource dataSource=null;
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(sql)) {
-            Duration durationBetweenLoginLogout = Duration.between(loginTime, logoutTime);
-            long minutes = durationBetweenLoginLogout.toMinutes();
-            statement.setInt(1, employeeId);
-            statement.setObject(2, loginTime);
-            statement.setObject(3, logoutTime);
-            statement.setInt(4, projectId);
-            statement.setLong(5, minutes);
-            statement.executeUpdate();
+        public void insertTimeWork(Connection con, int employeeId, LocalDate date, LocalTime loginTime, LocalTime logoutTime, int projectId) {
+            PreparedStatement ps = null;
+            try {
+                String query = "INSERT INTO czas_pracy (id_pracownika, data_zalogowania, data_wylogowania, id_projektu, czas) VALUES (?,?,?,?,?)";
+                ps = con.prepareStatement(query);
+                ps.setInt(1, employeeId);
+                ps.setDate(2, Date.valueOf(date));
+                ps.setTime(3, Time.valueOf(loginTime));
+                ps.setTime(4, Time.valueOf(logoutTime));
+                ps.setInt(5, projectId);
+
+                // obliczanie czasu pracy dla danego dnia
+                long workTime = Duration.between(loginTime, logoutTime).toMinutes();
+                ps.setLong(6, workTime);
+
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (ps != null) {
+                        ps.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void insertMonthlyTimeWork(Connection con, int employeeId, LocalDate date, int projectId) {
+            PreparedStatement ps = null;
+            try {
+                String query = "SELECT SUM(czas) FROM czas_pracy WHERE id_pracownika = ? AND MONTH(data_zalogowania) = MONTH(?) AND id_projektu = ?";
+                ps = con.prepareStatement(query);
+                ps.setInt(1, employeeId);
+                ps.setDate(2, Date.valueOf(date));
+                ps.setInt(3, projectId);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    int monthlyTime = rs.getInt(1);
+                    // wstawienie sumy czasu pracy dla danego miesiąca do tabeli
+                    // ...
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (ps != null) {
+                        ps.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    public void insertTotalTimeWork(Connection con, int employeeId, int projectId) {
+        PreparedStatement ps = null;
+        try {
+            String query = "SELECT SUM(czas) FROM czas_pracy WHERE id_pracownika = ? AND id_projektu = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, employeeId);
+            ps.setInt(2, projectId);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                int totalTime = rs.getInt(1);
+                System.out.println("Łączny czas pracy pracownika o ID " + employeeId + " dla projektu o ID " + projectId + " wynosi " + totalTime + " godzin.");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if(ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-
-
     }
+
+
+
+
+
 
 
 
